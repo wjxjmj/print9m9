d0a379b0456d5cbb3d9faa38c32dc4c81d2811f7
wjxjmj <wjxjmj@126.com>
2018-02-11 22:35:19 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,3 @@
+#include<stdio.h>
+#include <math.h>
+void main(i,_){while(i++<46)printf("%d*%d=%d  %c",((int)floor(0.6+sqrt(2*i-3.76))),((int)floor(i-0.9+0.5*floor(0.6+sqrt(2*i-3.76))-0.5*floor(0.6+sqrt(2*i-3.76))*floor(0.6+sqrt(2*i-3.76)))),((int)floor(0.6+sqrt(2*i-3.76)))*((int)floor(i-0.9+0.5*floor(0.6+sqrt(2*i-3.76))-0.5*floor(0.6+sqrt(2*i-3.76))*floor(0.6+sqrt(2*i-3.76)))),8+2*(int)(((int)floor(0.6+sqrt(2*i-3.76)))==((int)floor(i-0.9+0.5*floor(0.6+sqrt(2*i-3.76))-0.5*floor(0.6+sqrt(2*i-3.76))*floor(0.6+sqrt(2*i-3.76))))));}
